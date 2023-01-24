27151d502085ddb20ea13d7569da0a484fab0f55
John Evans <john.evans@digital.justice.gov.uk>
2023-01-24 08:32:41 +0000

    ✨ Add some scripts for listing commits on a repository

---
+++ github/README.md
@@ -0,0 +1,4 @@
+# Readme
+
+- Generate API token [here](https://github.com/settings/tokens)
+- API docs are [here](https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#list-commits)
---
+++ github/get-commits-on-main.bash
@@ -0,0 +1,5 @@
+curl \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $TOKEN"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/ministryofjustice/court-case-service/commits
