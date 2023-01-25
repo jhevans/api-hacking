255b5a21cb101559630cd9110d1c1cdf75a57c91
John Evans <john.evans@digital.justice.gov.uk>
2023-01-25 09:08:43 +0000

    ✨Find ticketIds using regex and print stats for every ticket found

--- github/index.js
+++ github/index.js
@@ -1,7 +1,6 @@
 const { Octokit } = require("@octokit/core");
 
 const octokit = new Octokit({ auth: process.env.TOKEN });
-const ticketId = "PIC-2613"
 
 function getTimeDelta(maxDate, minDate) {
     const ms = maxDate - minDate;
@@ -19,29 +18,42 @@ function getTimeDelta(maxDate, minDate) {
 function getStats(commits) {
     const commitDates = commits.map(item => new Date(item.commit.author.date));
 
-    const stats = {
+    return {
         commitDates,
         dateOfLastCommit: new Date(Math.max(...commitDates)),
         dateOfFirstCommit: new Date(Math.min(...commitDates)),
         cycleTime: getTimeDelta(Math.max(...commitDates), Math.min(...commitDates)),
         numberOfBugFixes: commits.filter(item => item.commit.message.includes("🐛")).length
-    }
-    return stats;
+    };
 }
 
+const ticketIdRegex = /PIC-\d{1,4}/;
 octokit.request("GET /repos/ministryofjustice/court-case-service/commits?per_page=100", {
     org: "octokit",
     type: "private",
 }).then((response) => {
 
-    const filteredData = response.data
-        .filter((item) => item.commit.message.includes(ticketId))
+    const tickets = new Map()
 
-    const stats = getStats(filteredData);
+    response.data.forEach(item => {
+        if (item.commit.message.match(ticketIdRegex) == null)
+            return
+        const ticketId = item.commit.message.match(ticketIdRegex)[0]
+        if (tickets.has(ticketId)) {
+            tickets.get(ticketId).push(item)
+        } else {
+            tickets.set(ticketId, [item])
+        }
+    })
 
-    console.log(`Of the ${response.data.length} last commits, ${filteredData.length} were associated with ticket ${ticketId}`)
-    console.log(`The first commit was on ${stats.dateOfFirstCommit}`)
-    console.log(`The last commit was on ${stats.dateOfLastCommit}`)
-    console.log(`Cycle time for ${ticketId} was ${(stats.cycleTime.days)} days, ${(stats.cycleTime.hours)} hours and ${(stats.cycleTime.minutes)} minutes`)
-    console.log(`There were ${stats.numberOfBugFixes} bug fixes associated with this ticket`)
+    tickets.forEach((commitArray, ticketId) => {
+        const stats = getStats(commitArray);
+
+        console.log(`Of the ${response.data.length} last commits, ${commitArray.length} were associated with ticket ${ticketId}`)
+        console.log(`The first commit was on ${stats.dateOfFirstCommit}`)
+        console.log(`The last commit was on ${stats.dateOfLastCommit}`)
+        console.log(`Cycle time for ${ticketId} was ${(stats.cycleTime.days)} days, ${(stats.cycleTime.hours)} hours and ${(stats.cycleTime.minutes)} minutes`)
+        console.log(`There were ${stats.numberOfBugFixes} bug fixes associated with this ticket`)
+        console.log(`------------------`)
+    })
 });
